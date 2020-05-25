c35fc8a599be66221fe1de256c8b3b8583b7237d
aliensmart <kaoua17@gmail.com>
2020-05-25 01:37:21 -0400

    show basic axis

--- maplo.py
+++ maplo.py
@@ -2,7 +2,14 @@
     matplotlib.pyplot is a python package used for 2D graphics
 """
 
-import matplotlib as plt 
+
+import matplotlib.pyplot as plt 
 
 x = [5, 10, 2]
 y = [19, 1, 6]
+
+plt.plot(x, y)
+plt.title('info')
+plt.xlabel('X axis')
+plt.ylabel('Y axis')
+plt.show()
